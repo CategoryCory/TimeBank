81daa398cf4298e0049c65f79dca919e7e515345
Cory Coward <cory@c2softwaresolutions.com>
2022-02-18 21:25:47 -0600

    Add fields to job category dto

--- TimeBank.API/Dtos/JobCategoryDto.cs
+++ TimeBank.API/Dtos/JobCategoryDto.cs
@@ -2,6 +2,8 @@
 {
     public class JobCategoryDto
     {
+        public int JobCategoryId { get; set; }
         public string JobCategoryName { get; set; }
+        public string JobCategorySlug { get; set; }
     }
 }
